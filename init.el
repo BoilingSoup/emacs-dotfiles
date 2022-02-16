956ef7f7331fdf5c3ec8ef79eba1ecfe9bb29b1a
BoilingSoup <boilingsoup@protonmail.com>
2022-02-16 15:25:06 -0800

    use-package ivy-rich

--- init.el
+++ init.el
@@ -53,7 +53,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key whick-key rainbow-delimiters doom-modeline counsel ivy use-package)))
+   '(ivy-rich which-key whick-key rainbow-delimiters doom-modeline counsel ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -107,3 +107,9 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package ivy-rich                                                  ; Use ivy-rich
+  :init
+  (ivy-rich-mode 1))

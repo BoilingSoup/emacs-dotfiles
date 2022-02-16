4595e245733cf9f1aab7e2b496616cb0e080e4c1
BoilingSoup <boilingsoup@protonmail.com>
2022-02-16 12:16:17 -0800

    use-package doom-modeline

--- init.el
+++ init.el
@@ -12,7 +12,7 @@
 
 (set-face-attribute 'default nil :font "Hack" :height 160)             ; Hack font
 
-(load-theme 'tango-dark)                                               ; Theme
+(load-theme 'wombat)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -41,7 +41,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(counsel ivy use-package)))
+ '(package-selected-packages '(doom-modeline counsel ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -76,3 +76,9 @@
 (use-package counsel                                                   ; Use counsel
   :after ivy
   :config (counsel-mode))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package doom-modeline                                             ; Use doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
